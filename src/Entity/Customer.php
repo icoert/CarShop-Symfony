e8ed8196eb8d1463385ac6b38e3105a3c9c974a6
icoert <silviuvaj@yahoo.com>
2020-12-06 14:18:03 +0200

    Customer Entity

---
+++ src/Entity/Customer.php
@@ -0,0 +1,96 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="customer")
+ * @ORM\Entity()
+ */
+class Customer
+{
+    /**
+     * @var int|null
+     * @ORM\Column(name="customerId", type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $customerId;
+
+    /**
+     * @var string|null
+     * @ORM\Column(name="name", type="string")
+     */
+    private $name;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="birthDate", type="datetime")
+     */
+    private $birthDate;
+
+    /**
+     * @return int|null
+     */
+    public function getCustomerId(): ?int
+    {
+        return $this->customerId;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     */
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getBirthDate(): \DateTime
+    {
+        return $this->birthDate;
+    }
+
+    /**
+     * @param \DateTime $birthDate
+     */
+    public function setBirthDate(\DateTime $birthDate): void
+    {
+        $this->birthDate = $birthDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param mixed $location
+     */
+    public function setLocation($location): void
+    {
+        $this->location = $location;
+    }
+
+    /**
+     * @var
+     * @ORM\Column(name="location", type="string", length=20)
+     */
+    private $location;
+
+}
